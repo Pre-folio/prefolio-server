8a80cc8b93d512713de2ad06357975796f9b4a11
chaeshee0908 <chaeshee0908@naver.com>
2022-12-26 19:43:51 +0900

    [init] initial commit

---
+++ src/main/java/prefolio/prefolioserver/PrefolioServerApplication.java
@@ -0,0 +1,13 @@
+package prefolio.prefolioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefolioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrefolioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prefolio/prefolioserver/PrefolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package prefolio.prefolioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefolioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
