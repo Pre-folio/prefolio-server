b0d71bcf7a442c55e5af1dd9b9ea0a1d9141c84d
chaeshee0908 <chaeshee0908@naver.com>
2023-01-03 01:00:33 +0900

    [chore] 연결 확인용 html 수정, swagger test api 작성

--- src/main/java/prefolio/prefolioserver/controller/AppController.java
+++ src/main/java/prefolio/prefolioserver/controller/AppController.java
@@ -1,4 +1,16 @@
 package prefolio.prefolioserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test/api")
 public class AppController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+
 }
