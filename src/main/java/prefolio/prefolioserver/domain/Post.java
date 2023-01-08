2efd81a5bdccd40a1105bd3903106d5a622d5409
chaeshee0908 <chaeshee0908@naver.com>
2023-01-08 19:04:46 +0900

    [feat] Post 모델 생성 #4

---
+++ src/main/java/prefolio/prefolioserver/domain/Post.java
@@ -0,0 +1,67 @@
+package prefolio.prefolioserver.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@Table(name = "Post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="userId", nullable = false)
+    private User user;
+
+    @Column
+    private String thumbnail;
+
+    @Column
+    private String title;
+
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    private Date startDate;
+
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    private Date endDate;
+
+    @Column
+    private Integer contribution;
+
+    @Column
+    private String tools;
+
+    @Column
+    private String part_tag;
+
+    @Column
+    private String act_tag;
+
+    @Column
+    private String contents;
+
+    @Column
+    private Integer hits;
+
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    private Date created_at;
+
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    private Date updated_at;
+
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    private Date deleted_at;
+
+}
