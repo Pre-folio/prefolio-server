f7cc83655e69d8b1f8af86c87b70d29ea33b2c58
chaeshee0908 <chaeshee0908@naver.com>
2023-01-11 18:59:34 +0900

    [feat] 게시물 조회 관련 DTO 작성 #11

--- src/main/java/prefolio/prefolioserver/dto/PostDTO.java
+++ src/main/java/prefolio/prefolioserver/dto/PostDTO.java
@@ -1,16 +1,26 @@
 package prefolio.prefolioserver.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import prefolio.prefolioserver.domain.Post;
 
 import java.sql.Date;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostDTO {
 
     private Long id;
     private String thumbnail;
     private String title;
+    private String startDate;
+    private String endDate;
+    private Integer contribution;
+    private String tools;
     private String partTag;
     private String actTag;
     private Integer hits;
@@ -20,6 +30,10 @@ public class PostDTO {
         this.id = post.getId();
         this.thumbnail = post.getThumbnail();
         this.title = post.getTitle();
+        this.startDate = post.getStartDate();
+        this.endDate = post.getEndDate();
+        this.contribution = post.getContribution();
+        this.tools = post.getTools();
         this.partTag = post.getPartTag();
         this.actTag = post.getActTag();
         this.hits = post.getHits();
