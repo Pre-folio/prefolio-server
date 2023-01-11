eab76efe26aa4332da15851429d63a397f022ef5
suhhyun <2835223@ewhain.net>
2023-01-12 01:19:31 +0900

    [feat] 닉네임 중복 확인 service 구현

--- src/main/java/prefolio/prefolioserver/service/UserService.java
+++ src/main/java/prefolio/prefolioserver/service/UserService.java
@@ -1,10 +1,12 @@
 package prefolio.prefolioserver.service;
 
 import org.springframework.stereotype.Service;
+import prefolio.prefolioserver.dto.CheckUserDTO;
 import prefolio.prefolioserver.dto.UserInfoDTO;
 
 @Service
 public interface UserService {
 
     UserInfoDTO.Response saveUserInfo(UserInfoDTO.Request userInfoRequest);
+    CheckUserDTO.Response checkUser(String nickname);
 }
--- src/main/java/prefolio/prefolioserver/service/UserServiceImpl.java
+++ src/main/java/prefolio/prefolioserver/service/UserServiceImpl.java
@@ -2,20 +2,26 @@ package prefolio.prefolioserver.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import prefolio.prefolioserver.domain.OAuth;
 import prefolio.prefolioserver.domain.User;
+import prefolio.prefolioserver.dto.CheckUserDTO;
 import prefolio.prefolioserver.dto.UserInfoDTO;
+import prefolio.prefolioserver.repository.AuthRepository;
 import prefolio.prefolioserver.repository.UserRepository;
+
+import java.util.Optional;
+
+import static java.lang.Boolean.TRUE;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
+    private final AuthRepository authRepository;
 
     @Override
     public UserInfoDTO.Response saveUserInfo(UserInfoDTO.Request userInfoRequest) {
-        System.out.println("AuthServiceImpl.saveUserInfo");
         User user = User.builder()
                 .type(userInfoRequest.getType())
                 .nickname(userInfoRequest.getNickname())
@@ -23,8 +29,26 @@ public class UserServiceImpl implements UserService {
                 .grade(userInfoRequest.getGrade())
                 .refreshToken(userInfoRequest.getRefreshToken())
                 .build();
+        OAuth oauth = OAuth.builder()
+                .isMember(TRUE)
+                .build();
         System.out.println("user Entity = " + user.getNickname());
         User savedUser = userRepository.saveAndFlush(user);
+        authRepository.saveAndFlush(oauth);
         return new UserInfoDTO.Response(savedUser);
     }
+
+
+    @Override
+    public CheckUserDTO.Response checkUser(String nickname) {
+        Optional<User> user = userRepository.findByNickname(nickname);
+//        return user.map(value -> new CheckUserDTO.Response(value.getNickname()))
+//                .orElseGet(() -> new CheckUserDTO.Response("null"));
+        if (user.isEmpty()) {
+            //new CheckUserDTO.Response().setIs_used(false);
+            return new CheckUserDTO.Response(false);
+        }
+        return new CheckUserDTO.Response(true);
+    }
+
 }
