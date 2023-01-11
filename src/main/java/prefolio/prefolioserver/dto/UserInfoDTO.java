8c9a8a0588f59fa7a8874d3fec7cb5eb535761d9
suhhyun <2835223@ewhain.net>
2023-01-11 18:57:31 +0900

    [feat] UserInfoDTO 작성 #7

---
+++ src/main/java/prefolio/prefolioserver/dto/UserInfoDTO.java
@@ -0,0 +1,49 @@
+package prefolio.prefolioserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import prefolio.prefolioserver.domain.User;
+
+import java.sql.Date;
+
+public class UserInfoDTO {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+
+        private String type;
+
+        private String nickname;
+
+        private String profileImage;
+
+        private Integer grade;
+
+        private String refreshToken;
+        /* Dto -> Entity */
+        public Request (User user) {
+            this.type = user.getType();
+            this.nickname = user.getNickname();
+            this.profileImage = user.getProfileImage();
+            this.grade = user.getGrade();
+            this.refreshToken = user.getRefreshToken();
+        }
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+
+        private Long userId;
+
+        /* Entity -> Dto */
+        public Response(User user) {this.userId = user.getId();}
+    }
+}
