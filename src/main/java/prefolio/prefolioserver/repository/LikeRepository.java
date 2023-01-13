239cc686d4f60c68eac35a79d3c95654524bbedf
suhhyun <2835223@ewhain.net>
2023-01-13 17:53:25 +0900

    [feat] UserInfo 관련 Repo 작성 #20

--- src/main/java/prefolio/prefolioserver/repository/LikeRepository.java
+++ src/main/java/prefolio/prefolioserver/repository/LikeRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface LikeRepository extends JpaRepository<Like, Long> {
     public Optional<Like> findByUserIdAndPostId(Long userId, Long postId);
     public Long countByPostId(Long postId);
+    public Optional<Long> countByUserId(Long userId);
 }
