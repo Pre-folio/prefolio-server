fd1953e49483907a89d277a18c8da57fd28d5522
chaeshee0908 <chaeshee0908@naver.com>
2023-01-15 15:32:57 +0900

    [feat] Security filter chain으로 변경 #23

--- src/main/java/prefolio/prefolioserver/config/WebSecurityConfig.java
+++ src/main/java/prefolio/prefolioserver/config/WebSecurityConfig.java
@@ -1,16 +1,100 @@
 package prefolio.prefolioserver.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
-@Configuration
-public class WebSecurityConfig implements WebMvcConfigurer {
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+    private static final String[] SwaggerPatterns = {
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+    // 인증 실패 또는 인증헤더가 전달받지 못했을 때 핸들러
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthenticationFilter authenticationFilter;
+    private final AccessDeniedHandler accessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(corsConfigurationSource())
+                .and()
+                .csrf().disable()
+                //예외처리 핸들러
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint) // 인증 실패
+                .accessDeniedHandler(accessDeniedHandler) // 인가 실패
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안 함
+                .and()
+                .httpBasic().disable()
+                //권한이 필요한 요청에 대한 설정
+                .authorizeHttpRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .requestMatchers(SwaggerPatterns).permitAll()
+                .requestMatchers("/auth/kakao").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .headers().frameOptions().disable();
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+        // 검토 필요
+    }
+
+    /** cors 설정 configuration bean */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        //로컬 react 개발 환경
+        configuration.addAllowedOriginPattern("*");
+        //서버 react 프론트 환경
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addExposedHeader("x-auth-token");
+        //내 서버의 응답 json 을 javascript에서 처리할수 있게 하는것(axios 등)
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    private CorsConfiguration getDefaultCorsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:3000", "https://api.prefolio.net", "https://prefolio.net"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        return configuration;
     }
 }
