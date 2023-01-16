bd91ba1e5f61d1581a28ba5a2f33c6dea0c1327a
suhhyun <2835223@ewhain.net>
2023-01-16 15:50:14 +0900

    [feat] Swagger 헤더 설정 #48

--- src/main/java/prefolio/prefolioserver/config/OpenAPIDefinitionConfiguration.java
+++ src/main/java/prefolio/prefolioserver/config/OpenAPIDefinitionConfiguration.java
@@ -1,6 +1,8 @@
 package prefolio.prefolioserver.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,6 +19,12 @@ public class OpenAPIDefinitionConfiguration {
     @Component
     @Profile("local")
     @OpenAPIDefinition(servers = @Server(url = "http://localhost:8080"))
+    @SecurityScheme(
+            name = "Bearer Authentication",
+            type = SecuritySchemeType.HTTP,
+            bearerFormat = "JWT",
+            scheme = "bearer"
+    )
     public static class LocalOpenAPIDefinitionConfiguration {
     }
 }
