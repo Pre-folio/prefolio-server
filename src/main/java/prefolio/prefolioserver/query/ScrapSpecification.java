a4b5e27784f8c5be2ee4be27eb5e0c18c5b19676
suhhyun <2835223@ewhain.net>
2023-01-21 00:34:19 +0900

    [feat] specification 작성 #76

---
+++ src/main/java/prefolio/prefolioserver/query/ScrapSpecification.java
@@ -0,0 +1,28 @@
+package prefolio.prefolioserver.query;
+
+import jakarta.persistence.criteria.*;
+import org.springframework.data.jpa.domain.Specification;
+import prefolio.prefolioserver.domain.Post;
+import prefolio.prefolioserver.domain.Scrap;
+import prefolio.prefolioserver.domain.User;
+
+public class ScrapSpecification {
+
+    public static Specification<Scrap> equalUserId(User user) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("user"), user);
+    }
+
+    public static  Specification<Scrap> likePostPartTag(String partTag) {
+        return (root, query, cb) -> {
+            Join<Post, Scrap> join = root.join("post", JoinType.INNER);
+            return cb.like(join.get("partTag"), '%' + partTag + '%');
+        };
+    }
+
+    public static Specification<Scrap> likePostActTag(String actTag) {
+        return (root, query, cb) -> {
+            Join<Post, Scrap> join = root.join("post", JoinType.INNER);
+            return cb.like(join.get("actTag"), '%' + actTag + '%');
+        };
+    }
+}
