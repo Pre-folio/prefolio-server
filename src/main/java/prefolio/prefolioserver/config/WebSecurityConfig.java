0ea5c35bcfbba436e015310d7f01204d4ae16123
chaeshee0908 <chaeshee0908@naver.com>
2023-01-21 19:50:14 +0900

    [fix] 허용 경로 이름 변경

--- src/main/java/prefolio/prefolioserver/config/WebSecurityConfig.java
+++ src/main/java/prefolio/prefolioserver/config/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .requestMatchers(SwaggerPatterns).permitAll()
-                .requestMatchers("/kakao/**").permitAll()
+                .requestMatchers("/kakao/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .headers().frameOptions().disable();
