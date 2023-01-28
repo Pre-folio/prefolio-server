8c8ddf160f165a640e856b8a5f8cd36f52761f7b
suhhyun <2835223@ewhain.net>
2023-01-28 14:00:48 +0900

    [add] 에러메시지 필드 추가 #107

--- src/main/java/prefolio/prefolioserver/error/CustomException.java
+++ src/main/java/prefolio/prefolioserver/error/CustomException.java
@@ -4,8 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+//@AllArgsConstructor
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
+    private Exception exception;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(ErrorCode errorCode, Exception exception) {
+        this.errorCode = errorCode;
+        this.exception = exception;
+    }
+
 }
