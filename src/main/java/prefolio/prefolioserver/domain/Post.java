f51cb025e4cafdf799d2910927fad8d1e1c8280e
suhhyun <2835223@ewhain.net>
2023-02-02 01:33:45 +0900

    [fix] 태그 다중 선택 기능 구현 #131

--- src/main/java/prefolio/prefolioserver/domain/Post.java
+++ src/main/java/prefolio/prefolioserver/domain/Post.java
@@ -8,6 +8,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLUpdate;
+import org.hibernate.annotations.Where;
+import org.springframework.transaction.annotation.Transactional;
 import prefolio.prefolioserver.dto.request.AddPostRequestDTO;
 
 import java.util.Date;
@@ -18,6 +22,8 @@ import java.util.List;
 @Setter
 @Table(name = "Post")
 @NoArgsConstructor
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE post SET deleted_at = CURRENT_TIMESTAMP where id = ?")
 public class Post {
 
     @Id
