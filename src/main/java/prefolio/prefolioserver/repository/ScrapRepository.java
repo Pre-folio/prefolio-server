1d70ff74d494c25d1d65f1859ca66b4bdd63b593
suhhyun <2835223@ewhain.net>
2023-02-11 21:23:20 +0900

    [fix] 총 좋아요 수, 스크랩 수 로직 수정 #152

--- src/main/java/prefolio/prefolioserver/repository/ScrapRepository.java
+++ src/main/java/prefolio/prefolioserver/repository/ScrapRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.transaction.annotation.Transactional;
 import prefolio.prefolioserver.domain.Scrap;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long>, JpaSpecificationExecutor<Scrap> {
@@ -15,8 +17,9 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long>, JpaSpecific
     Optional<Scrap> findByUserIdAndPostId(Long userId, Long postId);
     Long countByPostId(Long postId);
     Long countByUserIdAndPostId(Long userId, Long postId);
-    Optional<Long> countByUserId(Long userId);
     Page<Scrap> findAll(Specification<Scrap> spec, Pageable pageable);
+    List<Scrap> findAll(Specification<Scrap> spec);
+
     @Transactional
     void deleteByPostId(Long postId);
 
