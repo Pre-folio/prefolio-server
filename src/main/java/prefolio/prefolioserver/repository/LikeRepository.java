1ae1d5744bf951334f9ed050bb779ded5b5b59d3
suhhyun <2835223@ewhain.net>
2023-02-11 21:07:48 +0900

    [fix] cascade 적용 #151

--- src/main/java/prefolio/prefolioserver/repository/LikeRepository.java
+++ src/main/java/prefolio/prefolioserver/repository/LikeRepository.java
@@ -1,14 +1,15 @@
 package prefolio.prefolioserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import prefolio.prefolioserver.domain.Like;
-import prefolio.prefolioserver.domain.Post;
-import prefolio.prefolioserver.domain.User;
 
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    public Optional<Like> findByUserIdAndPostId(Long userId, Long postId);
-    public Long countByPostId(Long postId);
-    public Optional<Long> countByUserId(Long userId);
+    Optional<Like> findByUserIdAndPostId(Long userId, Long postId);
+    Long countByPostId(Long postId);
+    Optional<Long> countByUserId(Long userId);
+    @Transactional
+    void deleteByPostId(Long postId);
 }
